a272777adfc266942cbc6d2c57dfb83c9cf2a96f
Sofia Dubrowsky <sofidubrowsky@hotmail.com.ar>
2023-12-07 15:57:38 -0300

    navigation

--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,29 +1,39 @@
-import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  Pressable,
+} from "react-native";
 import products from "../data/products";
+import { useNavigation } from "@react-navigation/native";
 
 const ProductsScreen = () => {
-    return (
-        <FlatList
-            data={products}
-            renderItem={({ item }) => (
-            <View style={styles.itemContainer}>
-            <Image source={{ uri: item.image }} style={styles.image}/>
-            </View>
-            )}
-            numColumns={2}
-        />
-    );
-    }
+const navigation = useNavigation();
 
-    const styles = StyleSheet.create({
-        itemContainer: {
-          width: "50%",
-          padding: 1,
-        },
-        image: {
-          width: "100%",
-          aspectRatio: 1,
-        },
-      });
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <Pressable onPress={() => navigation.navigate('Product Details')} style={styles.itemContainer}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </Pressable>
+      )}
+      numColumns={2}
+    />
+  );
+};
 
-      export default ProductsScreen;
+const styles = StyleSheet.create({
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
+
+export default ProductsScreen;
